3f62d8a9fbda17e0cc898e8301bacce6cca15595
estherx <imyuxz@gmail.com>
2020-04-10 20:15:15 +0800

    hidden fail testing function.

--- test/test_app.py
+++ test/test_app.py
@@ -18,18 +18,18 @@ def test_hello_success(test_client):
     assert type(resp_body['msg']) == str
 
 
-def test_hello_fail(test_client):
-    """
-    Test url=/ response is different with assert value.
+# def test_hello_fail(test_client):
+#     """
+#     Test url=/ response is different with assert value.
 
-    @param test_client    (fixture) flask api app.
-    """
-    url = '/'
-    resp = test_client.get(url)
+#     @param test_client    (fixture) flask api app.
+#     """
+#     url = '/'
+#     resp = test_client.get(url)
 
-    assert resp.status_code == 200
+#     assert resp.status_code == 200
 
-    resp_body = resp.json
-    assert resp_body['msg'] == 'hello world'
-    assert type(resp_body['msg']) == str
+#     resp_body = resp.json
+#     assert resp_body['msg'] == 'hello world'
+#     assert type(resp_body['msg']) == str
 
